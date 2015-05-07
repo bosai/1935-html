9ba5432ee3994a97cf15e52f6e41ce9ec60a84ff
panfeilong321 <1271544691@qq.com>
2015-05-07 15:09:43 +0800

    Version 1.0

---
+++ nav.php
@@ -0,0 +1,83 @@
+<div class="navbar navbar-default" id="navbar">
+			<script type="text/javascript">
+				try{ace.settings.check('navbar' , 'fixed')}catch(e){}
+			</script>
+
+			<div class="navbar-container" id="navbar-container">
+				<div class="navbar-header pull-left">
+					<a href="#" class="navbar-brand">
+						<small>
+							<i class="icon-leaf"></i>
+							Ace Admin
+						</small>
+					</a><!-- /.brand -->
+				</div><!-- /.navbar-header -->
+
+				<div class="navbar-header pull-right" role="navigation">
+					<ul class="nav ace-nav">
+						<li class="grey">
+							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+								<i class="icon-tasks"></i>
+								<span class="badge badge-grey">公司1</span>
+							</a>
+						</li>
+                        <li class="grey">
+							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+								<i class="icon-tasks"></i>
+								<span class="badge badge-grey">公司2</span>
+							</a>
+						</li>
+                        <li class="grey">
+							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+								<i class="icon-tasks"></i>
+								<span class="badge badge-grey">公司3</span>
+							</a>
+						</li>
+                        <li class="grey">
+							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+								<i class="icon-tasks"></i>
+								<span class="badge badge-grey">公司4</span>
+							</a>
+						</li>
+
+
+						<li class="light-blue">
+							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
+								<img class="nav-user-photo" src="assets/avatars/user.jpg" alt="Jason's Photo">
+								<span class="user-info">
+									<small>Welcome,</small>
+									Jason
+								</span>
+
+								<i class="icon-caret-down"></i>
+							</a>
+
+							<ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+								<li>
+									<a href="#">
+										<i class="icon-cog"></i>
+										Settings
+									</a>
+								</li>
+
+								<li>
+									<a href="#">
+										<i class="icon-user"></i>
+										Profile
+									</a>
+								</li>
+
+								<li class="divider"></li>
+
+								<li>
+									<a href="#">
+										<i class="icon-off"></i>
+										Logout
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul><!-- /.ace-nav -->
+				</div><!-- /.navbar-header -->
+			</div><!-- /.container -->
+		</div>
---
+++ sidebar.php
@@ -0,0 +1,240 @@
+<div class="sidebar" id="sidebar">
+					<script type="text/javascript">
+						try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+					</script>
+
+					<div class="sidebar-shortcuts" id="sidebar-shortcuts">
+						<div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+							<button class="btn btn-success">
+								<i class="icon-signal"></i>
+							</button>
+
+							<button class="btn btn-info">
+								<i class="icon-pencil"></i>
+							</button>
+
+							<button class="btn btn-warning">
+								<i class="icon-group"></i>
+							</button>
+
+							<button class="btn btn-danger">
+								<i class="icon-cogs"></i>
+							</button>
+						</div>
+
+						<div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+							<span class="btn btn-success"></span>
+
+							<span class="btn btn-info"></span>
+
+							<span class="btn btn-warning"></span>
+
+							<span class="btn btn-danger"></span>
+						</div>
+					</div><!-- #sidebar-shortcuts -->
+
+					<ul class="nav nav-list">
+						<li class="active">
+							<a href="index.php">
+								<i class="icon-dashboard"></i>
+								<span class="menu-text"> 控制台 </span>
+							</a>
+						</li>
+
+						
+
+						<li>
+							<a href="shezhi.php" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 设置 </span>
+
+								
+							</a>
+                        <li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 用户管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="#" class="dropdown-toggle">
+										<i class="icon-double-angle-right"></i>
+
+										用户组
+										<b class="arrow icon-angle-down"></b>
+									</a>
+
+									<ul class="submenu">
+										<li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												本站用户
+											</a>
+										</li>    
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												第三方用户主
+											</a>
+										</li>  
+                                         <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												管理组
+											</a>
+										</li>    
+									</ul>
+                                    
+								</li>
+                                <li>
+									<a href="#" class="dropdown-toggle">
+										<i class="icon-double-angle-right"></i>
+
+										管理组
+										<b class="arrow icon-angle-down"></b>
+									</a>
+
+									<ul class="submenu">
+										<li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												角色管理
+											</a>
+										</li>    
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												管理员
+											</a>
+										</li>  
+                                             
+									</ul>
+                                    
+								</li>
+                                 
+
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 菜单管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+
+							<ul class="submenu">
+								<li>
+									<a href="#" class="dropdown-toggle">
+										<i class="icon-double-angle-right"></i>
+
+										前台菜单
+										<b class="arrow icon-angle-down"></b>
+									</a>
+
+									<ul class="submenu">
+										<li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												菜单管理
+											</a>
+										</li>    
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												菜单分类
+											</a>
+										</li>    
+									</ul>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 内容管理 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+									<ul class="submenu">
+										<li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												所有留言
+											</a>
+										</li>    
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												评论管理
+											</a>
+										</li>
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												文章管理
+											</a>
+										</li>   
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												分类管理
+											</a>
+										</li>  
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												页面管理
+											</a>
+										</li>
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												回收站
+											</a>
+										</li>        
+									</ul>
+                        </li>
+                        <li>
+							<a href="#" class="dropdown-toggle">
+								<i class="icon-desktop"></i>
+								<span class="menu-text"> 扩展工具 </span>
+
+								<b class="arrow icon-angle-down"></b>
+							</a>
+									<ul class="submenu">
+										<li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												幻灯片
+											</a>
+										</li>    
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												网站广告
+											</a>
+										</li>
+                                        <li>
+											<a href="#">
+												<i class="icon-leaf"></i>
+												友情链接
+											</a>
+										</li>   
+                            
+									</ul>
+                        </li>
+                        </ul>
+                            <!-- /.nav-list -->
+
+					<div class="sidebar-collapse" id="sidebar-collapse">
+						<i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+					</div>
+
+					<script type="text/javascript">
+						try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+					</script>
+				</div>
